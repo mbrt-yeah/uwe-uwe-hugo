6db3c9a52ef1653188eab26c489f1578b8427ee2
matthias.einbrodt@unibz.it <matthias.einbrodt@unibz.it>
2021-08-18 17:40:31 +0200

    Implements sticky and static mode

--- static/assets/js/uwe-uwe.js
+++ static/assets/js/uwe-uwe.js
@@ -260,3 +260,143 @@ UWE_UWE.TABS = (() => {
         init: init
     };
 })();
+
+UWE_UWE.TOC = (() => {
+    const config = {
+        "tocSelector": "#page-toc",
+        "tocButtonSelector": "#toc-button",
+    };
+
+    const cache = {
+        toc: undefined,
+        tocButton: undefined,
+        tocDisplayMode: undefined,
+    };
+
+    function init() {
+        cacheElements();
+        registerEventListeners()
+    }
+
+    function cacheElements() {
+        cache.toc = document.querySelector(config.tocSelector);
+        cache.tocButton = document.querySelector(config.tocButtonSelector);
+        cache.tocDisplayMode = document.body.getAttribute("data-uwe-uwe-toc-display-mode");
+    }
+
+    function registerEventListeners() {
+
+        if (cache.tocDisplayMode === "sticky") {
+            const tocLinks = cache.toc.querySelectorAll("a");
+
+            for (const tocLink of tocLinks) {
+                tocLink.addEventListener("click", function(e) {
+                    if ( !isTocInStickyMode() ) {
+                        return;
+                    }
+
+                    toggleStickyToc();
+                });
+            }
+
+            document.onkeydown = function(e) {
+                e = e || window.event;
+
+                if ( !e || e.key !== "Escape" || !isTocInStickyMode() ) { 
+                    return;
+                }
+
+                toggleStickyToc();
+            };
+        }
+
+        cache.tocButton.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            if (cache.tocDisplayMode === "static") {
+                return;
+            }
+
+            if (cache.tocDisplayMode === "mixed") {
+                if ( isElementInViewport(cache.toc) ) {
+                    return;
+                }
+
+                return toggleStickyToc();
+            }
+
+            if (cache.tocDisplayMode === "sticky") {
+                return toggleStickyToc();
+            }
+
+            throw new Error(`Unknown display mode for toc (${cache.tocDisplayMode}).`);
+        });
+    }
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function addClass(el, className) {
+        const classesSerialized = el.getAttribute("class");
+
+        if (classesSerialized === null || classesSerialized === undefined || classesSerialized === "") {
+            return el.setAttribute("class", className);
+        }
+
+        let classes = classesSerialized.trim().split(/\s/);
+        classes.push(className);
+        return el.setAttribute("class", classes.join(" ").trim());
+    }
+
+    function indexOfClass(el, className) {
+        const classesSerialized = el.getAttribute("class");
+
+        if (classesSerialized === null || classesSerialized === undefined || classesSerialized === "") {
+            return -1;
+        }
+
+        return classesSerialized.trim().split(/\s/).indexOf(className);
+    }
+
+    function isTocInStickyMode() {
+        return indexOfClass(cache.toc, "sticky") !== -1;
+    }
+
+    function removeClass(el, className) {
+        let classesSerialized = el.getAttribute("class");
+
+        if (classesSerialized === null || classesSerialized === undefined || classesSerialized === "") {
+            return el.removeAttribute("class");
+        }
+
+        classesSerialized = classesSerialized.replace(className, "").trim();
+
+        if (classesSerialized === "") {
+            return el.removeAttribute("class");
+        }
+
+        return el.setAttribute("class", classesSerialized);
+    }
+
+    function toggleStickyToc() {
+        const idx = indexOfClass(cache.toc, "sticky");
+
+        if (idx === -1) {
+            return addClass(cache.toc, "sticky");
+        }
+
+        return removeClass(cache.toc, "sticky");
+    }
+
+    return {
+        init: init
+    };
+})();
